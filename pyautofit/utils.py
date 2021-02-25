86d464a4a435e54065afebfe3721b72f59a62229
elshafiei <78775988+elshafiei@users.noreply.github.com>
2021-02-25 15:36:03 +1100

    Update utils.py
    added query_impala_table method

--- pyautofit/utils.py
+++ pyautofit/utils.py
@@ -1,6 +1,11 @@
 import numpy as np
 from sklearn.metrics import auc, roc_curve, roc_auc_score
 
+import subprocess
+import io
+import json
+import pandas as pd
+
 __all__ = ['woe', 'iv', 'gini']
 
 def woe(g_pct, b_pct):
@@ -18,3 +23,15 @@ def gini(y_true, y_pred, sample_weight=None):
     roc_auc = roc_auc_score(y_true, y_pred, sample_weight)
     result = 2 * roc_auc - 1
     return result
+
+
+def query_impala_table(qry, impala_server = 'asprlxhd401', schema='autelco_poc_db', sep='\t'):
+    cmd = ['impala-shell', '-k', '-B', '--print_header', '-i', impala_server, '-q', qry, '-d', schema]
+    try:
+        ret = subprocess.check_output(cmd, encoding="utf-8")
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+        return False
+
+    df = pd.read_csv(io.StringIO(ret), sep=sep)
+    return df
